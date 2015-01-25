bd900810cf80d06e06099661a1f6867796def07a
Martin Johns <martin.johns@live.de>
2015-01-25 13:17:20 +0100

    Switched used framework to .NET 4.0 and removed usage of Roslyn
    This resolves #1 and #3.

--- DatedTodo/Program.cs
+++ DatedTodo/Program.cs
@@ -2,96 +2,99 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.CodeAnalysis.Text;
+using System.Xml.Linq;
 
 namespace DatedTodo
 {
     public static class Program
     {
-        private static Regex Regex = new Regex(@"^\s*TODO DUE (?<date>\d{4}-\d{2}-\d{2}):*\s*(?<text>.*)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex Regex = new Regex(@"//\s*TODO DUE (?<date>\d{4}-\d{2}-\d{2}):?\s*(?<text>.*)$", RegexOptions.Multiline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private static TimeSpan WarningSpan = TimeSpan.FromDays(-5);
 
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            var sw = new Stopwatch();
-            sw.Start();
+            var projectPath = args.First();
 
-            MainAsync(args).Wait();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-            sw.Stop();
-            
-            Console.WriteLine("DatedTodo took " + sw.ElapsedMilliseconds + "ms");
-            return 0;
-        }
-
-        public static async Task MainAsync(string[] args)
-        {
-            var workspace = MSBuildWorkspace.Create();
-            var project = await workspace.OpenProjectAsync(args.First());
-
-            foreach (var document in project.Documents)
+            var project = XDocument.Load(projectPath);
+            var csharpFiles = GetCSharpFiles(project);
+            foreach (var file in csharpFiles)
             {
-                var commentTrivias = await GetSingleLineCommentTrivias(document);
-                foreach (var commentTrivia in commentTrivias)
+                var filePath = Path.Combine(Path.GetDirectoryName(projectPath), file);
+                var content = File.ReadAllText(filePath);
+                var matches = Regex.Matches(content);
+                foreach (Match match in matches)
                 {
-                    var comment = commentTrivia.ToString().Substring(2);
-                    var match = Regex.Match(comment);
-                    if (match.Success)
-                    {
-                        var dueDate = DateTime.ParseExact(match.Groups["date"].Value, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                        var text = match.Groups["text"].Value;
+                    var dueDate = DateTime.ParseExact(match.Groups["date"].Value, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                    var text = match.Groups["text"].Value.Trim();
 
-                        if (dueDate <= DateTime.Today)
-                        {
-                            Error(commentTrivia, dueDate, text);
-                        }
-                        else if (dueDate.Add(WarningSpan) <= DateTime.Today)
-                        {
-                            Warning(commentTrivia, dueDate, text);
-                        }
+                    if (dueDate <= DateTime.Today)
+                    {
+                        Error(filePath, GetTextPosition(content, match.Index), dueDate, text);
+                    }
+                    else if (dueDate.Add(WarningSpan) <= DateTime.Today)
+                    {
+                        Warning(filePath, GetTextPosition(content, match.Index), dueDate, text);
                     }
                 }
             }
+
+            stopwatch.Stop();
+            Console.WriteLine("DatedTodo took " + stopwatch.ElapsedMilliseconds + "ms");
         }
 
-        private static async Task<IEnumerable<SyntaxTrivia>> GetSingleLineCommentTrivias(Document document)
+        private static IEnumerable<string> GetCSharpFiles(XDocument doc)
         {
-            var root = await document.GetSyntaxRootAsync();
-            var result = root.DescendantTrivia().Where(x => x.CSharpKind() == SyntaxKind.SingleLineCommentTrivia);
-            return result;
+            return
+                doc.Root
+                    .DescendantNodes()
+                    .OfType<XElement>()
+                    .Where(x => x.Name == "{http://schemas.microsoft.com/developer/msbuild/2003}Compile")
+                    .Select(x => x.Attribute("Include"))
+                    .Where(x => x != null)
+                    .Select(x => x.Value);
         }
 
-        private static LinePosition GetStartLinePosition(SyntaxTrivia trivia)
+        private static TextPosition GetTextPosition(string str, int pos)
         {
-            return trivia.SyntaxTree.GetLineSpan(trivia.Span).Span.Start;
+            var line = 1;
+            var column = 0;
+            for (var i = 0; i <= pos; i++)
+            {
+                column++;
+
+                if (str[i] == '\n')
+                {
+                    line++;
+                    column = 0;
+                }
+            }
+
+            return new TextPosition(line, column);
         }
 
-        private static void Error(SyntaxTrivia trivia, DateTime dueDate, string message)
+        private static void Error(string filePath, TextPosition pos, DateTime dueDate, string message)
         {
-            var position = GetStartLinePosition(trivia);
             Console.WriteLine(
                 "{0}({1},{2}): error: Todo due {3:yyyy-MM-dd}: {4}",
-                trivia.SyntaxTree.FilePath,
-                position.Line + 1,
-                position.Character + 1,
+                filePath,
+                pos.Line,
+                pos.Column,
                 dueDate,
                 message);
         }
-
-        private static void Warning(SyntaxTrivia trivia, DateTime dueDate, string message)
+        private static void Warning(string filePath, TextPosition pos, DateTime dueDate, string message)
         {
-            var position = GetStartLinePosition(trivia);
             Console.WriteLine(
                 "{0}({1},{2}): warning: Todo due {3:yyyy-MM-dd}: {4}",
-                trivia.SyntaxTree.FilePath,
-                position.Line + 1,
-                position.Character + 1,
+                filePath,
+                pos.Line,
+                pos.Column,
                 dueDate,
                 message);
         }
---
+++ DatedTodo/TextPosition.cs
@@ -0,0 +1,15 @@
+﻿namespace DatedTodo
+{
+    internal struct TextPosition
+    {
+        public TextPosition(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public int Line { get; set; }
+
+        public int Column { get; set; }
+    }
+}
